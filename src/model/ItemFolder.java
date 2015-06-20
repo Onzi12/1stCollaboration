50267455531a7cad56594c0ade708c4d59e112cd
On Albilia <onzi12@gmail.com>
2015-06-20 13:57:42 +0300

    change computers , alot more to do

--- src/model/ItemFolder.java
+++ src/model/ItemFolder.java
@@ -22,63 +22,7 @@ public class ItemFolder extends Item {
 	private DefaultMutableTreeNode treeNode;
 	
 	
-	//Server Only
-	public static ItemFolder DBgetRoot(int userID) throws SQLException {
-		ItemFolder root;
-		Connection con = Server.getConnection();
-		try(Statement stmt  = con.createStatement() ) 
-		{
-			ResultSet rs = stmt.executeQuery("SELECT folderID FROM folder WHERE userID = "+userID+" AND name = 'root'");
-			if(rs.next())
-				{
-				root = new ItemFolder();
-				root.setParent(null);
-				root.setName("root");
-				root.setId( rs.getInt("folderID") );
-				root.createContent();
-				}
-			else 
-				{
-				System.err.println("ERROR: root folder for user" +userID +" is not exist!! ");
-				return null; 
-				}
-		}
-		return root;
-	}
 	
-	
-	private void createContent() {
-		Connection con = Server.getConnection();
-		try( Statement stmt  = con.createStatement() ) 
-		{
-			ResultSet rs = stmt.executeQuery("SELECT fileId FROM userfiles WHERE folderID = "+getId());
-			while(rs.next())
-			  {
-				//try(PreparedStatement st = con.prepareStatement(")) continue tomorrow..
-			  }
-		}
-		
-		stmt2 = connection.prepareStatement("SELECT * FROM userfiles,file where userId = ? and file.id = userfiles.fileId");//where userId = ?
-		stmt2.setInt(1, user.getID());
-		rs2 = stmt2.executeQuery();
-	}
-
-
-	//Used by the server only
-	public ItemFolder DBgetFolder(int userID, int folderID) {
-		return null;
-	}
-	
-	//Used by the server only
-	public ItemFolder DBgetFolder(int userID, int folderID,String folderName) {
-		return null;
-	}
-
-	//Used by the server only
-	public Item[] DBgetFolderItems(int userID, int folderID) {
-		return null;
-	}
-
 	public void addItem(Item item) {
 			contents.add(item);
 		}
